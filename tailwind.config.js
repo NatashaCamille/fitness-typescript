23e27b3084f1f078a3eb73baf74aff51273d0523
NatashaCamille <nnalubega@outlook.com>
2023-07-09 20:41:29 +0300

    Added som styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,11 @@ module.exports = {
         dmsans: ["DM Sans", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
       },
+      screens: {
+        xs: "480px",
+        sm: "768px",
+        md: "1060px",
+      },
     },
   },
   plugins: [],
