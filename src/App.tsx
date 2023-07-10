52a46a3aa5ce2ec06afe07b2dcc5e78164beed2b
NatashaCamille <nnalubega@outlook.com>
2023-07-11 00:04:52 +0300

    Feat: updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
-   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
  return (
     <div className="app"> 
