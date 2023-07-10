2f05dacbec1c34804d15a263f1253c609bf7fda6
NatashaCamille <nnalubega@outlook.com>
2023-07-11 00:03:33 +0300

    Feat: updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
-  
+   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
  return (
     <div className="app"> 
      Now
