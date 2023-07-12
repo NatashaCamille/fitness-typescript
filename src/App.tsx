1464ffe23a7ef035ed57e54a92a73c162124c208
NatashaCamille <nnalubega@outlook.com>
2023-07-12 11:38:05 +0300

    Feat: some edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Navbar from "@/scenes/navbar";
+
 function App() {
- return (
-    <div className="app"> 
-     Now
-    </div>
-  )
+ return 
+    <div className="app bg-gray-20">
+      <Navbar />
+    </div>;
+
 }
 
 export default App
