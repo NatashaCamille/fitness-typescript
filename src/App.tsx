57b82008ed870bcf968d85a1a83bebc8454a6c45
NatashaCamille <nnalubega@outlook.com>
2023-07-12 10:47:34 +0300

    Feat: classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,4 @@
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
-    SelectedPage.Home
-  );
-  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setIsTopOfPage(true);
-        setSelectedPage(SelectedPage.Home);
-      }
-      if (window.scrollY !== 0) setIsTopOfPage(false);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
  return (
     <div className="app"> 
      Now
