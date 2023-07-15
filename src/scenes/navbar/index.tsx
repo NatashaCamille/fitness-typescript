13f29bb2fb2a96ed7071d7d3c6c10717c92b1450
NatashaCamille <nnalubega@outlook.com>
2023-07-15 22:10:33 +0300

    Feat: updates

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,22 +1,23 @@
 import { useState } from "react"
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, LinkIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 
  type Props = {};
 
 const Navbar = (props: Props) => {
   const flexbetween = "flex items-center justify-between";
+
   return (
     <nav>
         <div className={`${flexbetween} fixed top-0 z-30 w-full py-6`}>
           <div className={`${flexbetween} mx-auto w-5/6`}>
             <div className={`${flexbetween} w-full gap-16`}>
-              
+
               <img alt="logo" src={Logo} />
 
               <div className={`${flexbetween} w-full`}>
                 <div className={`${flexbetween} gap-8 text-sm`}>
-                  <p>Home</p>
+                  <Link page="Home" selectedPage={selectedPage}/>
                   <p>Benefits</p>
                   <p>Our Classes</p>
                   <p>Contact</p>
