19a2bcf66660a320493ded2af9c076f7a3db95f4
NatashaCamille <nnalubega@outlook.com>
2023-07-18 23:40:27 +0300

    Updates

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -5,16 +5,31 @@ import Logo from "@/assets/Logo.png";
 type Props = {}
 
 const Navbar = (props: Props) => {
-  const flexBetween = "Flex items-center justify-between";
+  const flexBetween = "Flex items-center justify-center";
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
+      <div className=""
         >
           <div className={`${flexBetween} mx-auto w-5/6`}>
+            <div className={`${flexBetween} w-full gap-16`}>
+              
+              <img alt="logo" src={Logo} />
 
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <p>Home</p>
+                  <p>Benefits</p>
+                  <p>Our classes</p>
+                  <p>Contact Us</p>
+                </div>
+                <div>
+                  <p>Sign Up</p>
+                  <button>Become A Member</button>
+                </div>
+              </div>
+            </div>
           </div>
-
       </div>
     </nav>
   )
