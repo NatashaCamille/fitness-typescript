4193990a988a14fa06e88bfcb0b5d42ee6877b72
NatashaCamille <nnalubega@outlook.com>
2023-07-18 17:54:01 +0300

    Some updates

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+  const flexBetween = "Flex items-center justify-between";
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
+
+      </div>
+    </nav>
   )
 }
 
-export default Navbar;
+export default Navbar; 
